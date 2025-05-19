2ac6872afbd59804a11a16b49ee563612729f451
Corentin <leguen.corentin@protonmail.com>
2025-05-19 11:41:51 -0400

    Update BookService.java
    Remove unnecessary @Transactional annotation.

--- src/main/java/com/librairie/catalogue/service/BookService.java
+++ src/main/java/com/librairie/catalogue/service/BookService.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@Transactional
 @Service
 public class BookService {
     private final BookRepository bookRepository;
